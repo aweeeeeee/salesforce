1df1ad64a1d4cda1dd3f21a0c5d8d8c61bb017f6
Bicomong <normanorman14@gmail.com>
2021-10-14 13:00:16 +0800

    create lwc display data using datatable

--- force-app/main/default/lwc/opportunityDynamic/opportunityDynamic.js
+++ force-app/main/default/lwc/opportunityDynamic/opportunityDynamic.js
@@ -1,9 +1,24 @@
 import { LightningElement, api, wire } from 'lwc';
 import getOpportunities from '@salesforce/apex/OpportunityData.getOpportunities';
+import getContactList from '@salesforce/apex/OpportunityData.getContactList';
+
+const columns = [
+    { label: 'First Name', fieldName: 'FirstName' },
+    { label: 'Last Name', fieldName: 'LastName' },
+    { label: 'Title', fieldName: 'Title' },
+    { label: 'Phone', fieldName: 'Phone', type: 'phone' },
+    { label: 'Email', fieldName: 'Email', type: 'email' }
+];
+
 export default class OpportunityDynamic extends LightningElement {
     @api recordId;
     //@wire(getOpportunities,{ accId: '$recordId' })
     //opportunities;
 
+    error;
+    columns = columns;
+
     @wire(getOpportunities) opportunities;
+
+    @wire(getContactList) contacts;
 }
