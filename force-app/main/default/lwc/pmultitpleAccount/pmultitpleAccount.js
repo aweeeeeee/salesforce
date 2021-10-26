1c9fa05a566632fd4e2a56ae584aeff48415f754
Bicomong <normanorman14@gmail.com>
2021-10-26 14:49:50 +0800

    fix multipla account create on lwc

--- force-app/main/default/lwc/pmultitpleAccount/pmultitpleAccount.js
+++ force-app/main/default/lwc/pmultitpleAccount/pmultitpleAccount.js
@@ -1,9 +1,11 @@
 import { LightningElement, track } from 'lwc';
-import saveAccount from '@salesforce/apex/AccountCreationController.createAccounts';
+import saveAccounts from '@salesforce/apex/AccountCreationController.createAccounts';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class PmultitpleAccount extends LightningElement {
     @track keyIndex = 0;
+    @track error;
+    @track message;
     @track accountRecList = [
         {
             Name: '',
@@ -22,14 +24,54 @@ export default class PmultitpleAccount extends LightningElement {
     }
 
     changeHandler(event){
-
+        if(event.target.name ==='accName'){
+            this.accountRecList[event.target.accessKey].Name = event.target.value;
+        } else if(event.target.name ==='accIndustry'){
+            this.accountRecList[event.target.accessKey].Industry = event.target.value;
+        } else if(event.target.name ==='accPhone'){
+            this.accountRecList[event.target.accessKey].Phone = event.target.value;
+        }
     }
 
     saveMultipleAccounts(){
+        console.log('test');
+        saveAccounts({ accountList : this.accountRecList})
+            .then(result => {
+                this.message = result;
+                this.error = undefined;
+                this.accountRecList.forEach(function(item){
+                    item.Name='';
+                    item.Industry='';
+                    item.Phone='';
+                });
 
+                if(this.message !== undefined){
+                    this.dispatchEvent(
+                        new ShowToastEvent({
+                            title: 'Success',
+                            message: 'Accounts created',
+                            variant: 'success',
+                        }),
+                    );
+                }
+            })
+            .catch(error => {
+                this.message = undefined;
+                this.error = error;
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'error creating record',
+                        message: error.body.message,
+                        variant: 'error',
+                    })
+                );
+            });
     }
 
     removeRow(event){
-
+        if(this.accountRecList.length>=1){
+            this.accountRecList.splice(event.target.accessKey,1);
+            this.keyIndex-1;
+        }
     }
 }
