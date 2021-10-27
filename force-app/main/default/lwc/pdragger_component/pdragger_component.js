36617fac98b7c98b406a5abead2e53f3e088bb3c
Bicomong <normanorman14@gmail.com>
2021-10-27 14:29:02 +0800

    create drag and drop in lwc

---
+++ force-app/main/default/lwc/pdragger_component/pdragger_component.js
@@ -0,0 +1,9 @@
+import { LightningElement, wire } from 'lwc';
+import getAccounts from '@salesforce/apex/Accountcls.getAccountList';
+
+export default class Pdragger_component extends LightningElement {
+    @wire(getAccounts) accounts;
+    handleDragStart(event){
+        event.dataTransfer.setData("account_id", event.target.dataset.item);
+    }
+}
